7d0a2f9a41216d817c85b56f3db256816fc8f8c9
ttus612 <thanhtu6122002@gmail.com>
2023-09-13 13:24:23 +0700

    function login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\Admin\Users\LoginController;
+use \App\Http\Controllers\Admin\MainController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('admin/users/login', [LoginController::class,  'index']);
+Route::post('admin/users/login/store', [LoginController::class,  'store']);
+
+Route::get('admin/main', [MainController::class, 'index'])->name('admin');
