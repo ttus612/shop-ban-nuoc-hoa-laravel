be7d5a261ac7690f7a40d95edb8826020cc79fec
ttus612 <thanhtu6122002@gmail.com>
2023-09-14 23:16:21 +0700

    function add danh mục, điều chỉnh router, view dashboard

---
+++ resources/views/admin/menu/add.blade.php
@@ -0,0 +1,75 @@
+@extends('admin.main')
+@section('head')
+    <script src="https://cdn.ckeditor.com/ckeditor5/39.0.2/classic/ckeditor.js"></script>
+    <style>
+        #container {
+            width: 1000px;
+            margin: 20px auto;
+        }
+        .ck-editor__editable[role="textbox"] {
+            /* editing area */
+            min-height: 200px;
+        }
+        .ck-content .image {
+            /* block images */
+            max-width: 80%;
+            margin: 20px auto;
+        }
+    </style>
+@endsection
+@section('content')
+    <form action="" method="POST">
+
+        <div class="card-body">
+
+            <div class="form-group">
+                <label for="menu">Tên danh mục</label>
+                <input type="text" name="menu" class="form-control" id="menu" placeholder="Enter name">
+            </div>
+
+            <div class="form-group">
+                <label>Danh mục </label>
+                <select class="form-control" name="parent_id">
+                    <option value="0">Danh mục cha</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>Mô tả</label>
+                <textarea name="description" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label>Chi tiết danh mục:</label>
+                <textarea name="content" id="content" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label>Kích hoạt</label>
+                <div class="custom-control custom-radio">
+                    <input class="custom-control-input" type="radio" value="1" id="active" name="active" checked="">
+                    <label for="active" class="custom-control-label">Có</label>
+                </div>
+                <div class="custom-control custom-radio">
+                    <input class="custom-control-input" type="radio" value="0" id="no_active" name="active" >
+                    <label for="no_active" class="custom-control-label">Không</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Tạo danh mục</button>
+        </div>
+    </form>
+@endsection
+
+@section('footer')
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#content' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Admin\Users\LoginController;
 use \App\Http\Controllers\Admin\MainController;
+use \App\Http\Controllers\Admin\MenuController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +15,22 @@ use \App\Http\Controllers\Admin\MainController;
 |
 */
 
-Route::get('admin/users/login', [LoginController::class,  'index']);
+Route::get('admin/users/login', [LoginController::class,  'index'])->name('login');
 Route::post('admin/users/login/store', [LoginController::class,  'store']);
 
-Route::get('admin/main', [MainController::class, 'index'])->name('admin');
+Route::middleware(['auth'])->group(function () {
+
+    Route::prefix('admin')->group(function (){
+        #Main: admin/...
+        Route::get('/', [MainController::class, 'index'])->name('admin');
+        Route::get('main', [MainController::class, 'index']);
+
+        #Menus: admin/menus/...
+        Route::prefix('menus')->group(function () {
+            Route::get('add', [MenuController::class, 'create']);
+        });
+    });
+});
+
+
+
